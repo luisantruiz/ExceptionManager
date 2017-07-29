ecdbef82e7cb879067ff1bf8e6f3ae703f02f368
Tony Ruiz <luisantruiz@gmail.com>
2017-07-29 12:30:02 -0600

    Add project files.

---
+++ ExceptionManager.Tests/ExceptionManagerTests.cs
@@ -0,0 +1,49 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace ExceptionManager.Tests
+{
+    [TestClass]
+    public class Tests
+    {
+        [TestMethod]
+        public void ShouldGetDeepestException()
+        {
+            var innerException = new Exception("There was another error");
+            var exception = new Exception("There was an error", innerException);
+
+            Exception exceptionReturned = ExceptionManager.GetDeepestException(exception);
+            Assert.AreEqual(exceptionReturned.Message, innerException.Message);
+        }
+
+        [TestMethod]
+        public void ShouldGetDeepestExceptionMessage()
+        {
+            var innerException = new Exception("There was another error");
+            var exception = new Exception("There was an error", innerException);
+
+            string messageReturned = ExceptionManager.GetDeepestExceptionMessage(exception);
+            Assert.AreEqual(messageReturned, innerException.Message);
+        }
+
+        [TestMethod]
+        public void ShouldConcatAllInnerExceptionMessages()
+        {
+            var innerException = new Exception("There was another error");
+            var exception = new Exception("There was an error", innerException);
+
+            string messageReturned = ExceptionManager.Concatener.ConcatAllInnerExceptionMessages(exception);
+            Assert.AreEqual(messageReturned, $"{exception.Message}, {innerException.Message}");
+        }
+
+        [TestMethod]
+        public void ShouldConcatAllInnerExceptionMessagesWithSeparator()
+        {
+            var innerException = new Exception("There was another error");
+            var exception = new Exception("There was an error", innerException);
+
+            string messageReturned = ExceptionManager.Concatener.ConcatAllInnerExceptionMessages(exception, " |");
+            Assert.AreEqual(messageReturned, $"{exception.Message} | {innerException.Message}");
+        }
+    }
+}
---
+++ ExceptionManager/ExceptionManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace ExceptionManager
+{
+    public static class ExceptionManager
+    {
+        public static string GetDeepestExceptionMessage(Exception exception)
+        {
+            var deepestException = GetDeepestException(exception);
+            return Common.GetExceptionMessage(deepestException);
+        }
+
+        public static Exception GetDeepestException(Exception exception)
+        {
+            Exception ex = exception;
+            while (ex.InnerException != null)
+                ex = ex.InnerException;
+
+            return ex;
+        }
+
+        public static string GetInnerExceptionMessage(Exception exception)
+        {
+            return GetInnerException(exception)?.Message;
+        }
+
+        public static Exception GetInnerException(Exception exception)
+        {
+            return exception.InnerException;
+        }
+        
+        public static class Concatener
+        {
+            public static string ConcatAllInnerExceptionMessages(Exception exception, string separator = ",")
+            {
+                string messages = Common.GetExceptionMessage(exception);
+
+                var baseException = exception;
+                while (baseException.InnerException != null)
+                {
+                    baseException = baseException.InnerException;
+                    messages += $"{separator} {Common.GetExceptionMessage(baseException)}";
+                }
+
+                exception = new Exception(messages);
+
+                return Common.GetExceptionMessage(exception);
+            }
+        }
+
+        protected static class Common
+        {
+            public static string GetExceptionMessage(Exception exception)
+            {
+                return exception?.Message;
+            }
+        }
+    }
+}
